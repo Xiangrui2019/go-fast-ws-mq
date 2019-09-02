bcf833e5ba2bb52add7effb6bd0e5e8092fc99f6
Go Go Go <xiangrui@aiursoft.com>
2019-09-02 18:20:45 +0800

    add alls

--- api/listen.go
+++ api/listen.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"go-fast-ws-mq/serializer"
 	"go-fast-ws-mq/services"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +11,15 @@ import (
 func ListenChannel(context *gin.Context) {
 	service := services.ListenChannelService{}
 
-	if err := service.Listen(context); err != nil {
-		context.JSON(err.Code, err)
+	if err := context.ShouldBindQuery(&service); err != nil {
+		context.JSON(http.StatusBadRequest, &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "您的输入有误.",
+			Error:   err.Error(),
+		})
+	} else {
+		if err := service.Listen(context); err != nil {
+			context.JSON(err.Code, err)
+		}
 	}
 }
--- services/listen_channel_service.go
+++ services/listen_channel_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"go-fast-ws-mq/models"
 	"go-fast-ws-mq/modules"
 	"go-fast-ws-mq/serializer"
 
@@ -10,13 +11,30 @@ import (
 )
 
 type ListenChannelService struct {
+	ChannelId  int `form:"channel_id" json:"channel_id" binding:"required"`
+	ConnectKey int `form:"connect_key" json:"connect_key" binding:"required"`
 }
 
 func (service *ListenChannelService) Listen(context *gin.Context) *serializer.Response {
-	//channelId := context.Param("channelId")
-	//connectKey := context.Query("connectKey")
+	count := 0
+	err := models.DB.Model(&models.Channel{}).Where("id = ?", service.ChannelId).Count(&count).Error
 
-	err := modules.WebSocketModule.HandleRequest(context.Writer, context.Request)
+	if err != nil {
+		return &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "数据库出错.",
+			Error:   err.Error(),
+		}
+	}
+
+	if count == 0 {
+		return &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "无法找到对应的频道.",
+		}
+	}
+
+	err = modules.WebSocketModule.HandleRequest(context.Writer, context.Request)
 
 	if err != nil {
 		return &serializer.Response{
