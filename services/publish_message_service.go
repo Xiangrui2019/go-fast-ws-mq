bc0562fb73d3def84d70532445f5d53c60e4934a
Go Go Go <xiangrui@aiursoft.com>
2019-09-03 12:45:00 +0800

    add number

--- services/publish_message_service.go
+++ services/publish_message_service.go
@@ -3,7 +3,11 @@ package services
 import "go-fast-ws-mq/serializer"
 
 type PublishMessageService struct {
+	ChannelId  int
+	ConnectKey int
+	Message    string
 }
 
 func (service *PublishMessageService) Publish() *serializer.Response {
+	return nil
 }
