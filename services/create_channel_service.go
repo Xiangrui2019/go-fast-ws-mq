d29bc1b085872f870824a38c036c63131544e916
Go Go Go <xiangrui@aiursoft.com>
2019-09-04 19:14:30 +0800

    add create channel service

--- services/create_channel_service.go
+++ services/create_channel_service.go
@@ -1,8 +1,13 @@
 package services
 
+import (
+	"go-fast-ws-mq/models"
+	"go-fast-ws-mq/serializer"
+)
+
 type CreateChannelService struct {
 }
 
-func (service *CreateChannelService) Create() {
+func (service *CreateChannelService) Create() (*models.Channel, *serializer.Response) {
 
 }
