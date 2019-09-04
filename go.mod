c30c99cc8ef5a1f34cb108fdfdd306295cde7c3b
Go Go Go <xiangrui@aiursoft.com>
2019-09-04 20:39:48 +0800

    add create

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/jinzhu/gorm v1.9.10
 	github.com/joho/godotenv v1.3.0
+	github.com/satori/go.uuid v1.2.0
 	gopkg.in/go-playground/validator.v8 v8.18.2
 	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376
 )
--- services/create_channel_service.go
+++ services/create_channel_service.go
@@ -3,7 +3,9 @@ package services
 import (
 	"go-fast-ws-mq/models"
 	"go-fast-ws-mq/serializer"
-	"github.com/satori/go.uuid"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type CreateChannelService struct {
@@ -11,10 +13,48 @@ type CreateChannelService struct {
 	Description string `form:"description" json:"description" binding:"required"`
 }
 
-func (service *CreateChannelService) Create() (*models.Channel, *serializer.Response) {
-	channel := &models.Channel{
-		Name: service.Name,
-		Description: service.Description,
-		ConnectKey: ,
+func (service *CreateChannelService) Vaild() *serializer.Response {
+	count := 0
+	err := models.DB.Model(&models.Channel{}).Where("name = ?", service.Name).Count(&count).Error
+
+	if err != nil {
+		return &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "数据库出错.",
+			Error:   err.Error(),
+		}
 	}
+
+	if count == 0 {
+		return &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "无法找到对应的频道.",
+		}
+	}
+
+	return nil
+}
+
+func (service *CreateChannelService) Create() (*models.Channel, *serializer.Response) {
+	if err := service.Vaild(); err != nil {
+		return nil, err
+	}
+
+	channel := &models.Channel{
+		Name:        service.Name,
+		Description: service.Description,
+		ConnectKey:  uuid.NewV4().String(),
+	}
+
+	result := models.DB.Create(&channel)
+
+	if result.Error != nil {
+		return nil, &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "数据库创建错误.",
+			Error:   result.Error.Error(),
+		}
+	}
+
+	return result.Value.(*models.Channel), nil
 }
