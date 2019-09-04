b0cbdf0019a1f2cedd4954254e1e162cc92ed10d
Go Go Go <xiangrui@aiursoft.com>
2019-09-04 21:05:04 +0800

    add create_Channel

--- api/channel.go
+++ api/channel.go
@@ -1,7 +1,25 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-fast-ws-mq/serializer"
+	"go-fast-ws-mq/services"
+	"go-fast-ws-mq/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CreateChannel(context *gin.Context) {
+	service := services.CreateChannelService{}
 
+	if err := context.ShouldBind(&service); err == nil {
+		if channel, err := service.Create(); err != nil {
+			context.JSON(err.Code, err)
+		} else {
+			context.JSON(http.StatusOK,
+				serializer.BuildPrivateChannelResponse(channel))
+		}
+	} else {
+		context.JSON(http.StatusBadRequest, utils.BuildErrorResponse(err))
+	}
 }
