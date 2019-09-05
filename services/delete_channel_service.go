a54241f404e9625013ede9fd73f9ba14189429b3
Go Go Go <xiangrui@aiursoft.com>
2019-09-06 06:46:17 +0800

    result

--- services/delete_channel_service.go
+++ services/delete_channel_service.go
@@ -12,12 +12,13 @@ type DeleteChannelService struct {
 }
 
 func (service *DeleteChannelService) Delete(context *gin.Context) *serializer.Response {
-	err := models.DB.Model(&models.Channel{}).Delete("id = ?", context.Param("id")).Error
+	err := models.DB.Model(&models.Channel{}).Where("id = ?", context.Param("id")).Delete(&models.Channel{}).Error
 
 	if err != nil {
 		return &serializer.Response{
 			Code:    http.StatusInternalServerError,
 			Message: "删除失败",
+			Error:   err.Error(),
 		}
 	}
 
