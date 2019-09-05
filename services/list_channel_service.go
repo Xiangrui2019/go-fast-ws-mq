67b6d6cfa34199b42ef7bef47ff6b1d72dc0b785
Go Go Go <xiangrui@aiursoft.com>
2019-09-06 06:11:34 +0800

    add alls

--- services/list_channel_service.go
+++ services/list_channel_service.go
@@ -3,11 +3,24 @@ package services
 import (
 	"go-fast-ws-mq/models"
 	"go-fast-ws-mq/serializer"
+	"net/http"
 )
 
 type ListChannelService struct {
 }
 
 func (service *ListChannelService) List() ([]*models.Channel, *serializer.Response) {
-	return make([]*models.Channel, 0), nil
+	var channels []*models.Channel
+
+	err := models.DB.Model(&models.Channel{}).Find(&channels).Error
+
+	if err != nil {
+		return nil, &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "数据库出错.",
+			Error:   err.Error(),
+		}
+	}
+
+	return channels, nil
 }
