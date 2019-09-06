851f94859ace56989cb7f3f842227dc8ee166cfc
Go Go Go <xiangrui@aiursoft.com>
2019-09-06 12:15:01 +0800

    add input checker

--- services/delete_channel_service.go
+++ services/delete_channel_service.go
@@ -11,7 +11,34 @@ import (
 type DeleteChannelService struct {
 }
 
+func (service *DeleteChannelService) Vaild(context *gin.Context) *serializer.Response {
+	count := 0
+
+	err := models.DB.Model(&models.Channel{}).Where("id = ?", context.Param("id")).Count(&count).Error
+
+	if err != nil {
+		return &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "数据库出错.",
+			Error:   err.Error(),
+		}
+	}
+
+	if count == 0 {
+		return &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "无法找到对应的记录.",
+		}
+	}
+
+	return nil
+}
+
 func (service *DeleteChannelService) Delete(context *gin.Context) *serializer.Response {
+	if err := service.Vaild(context); err != nil {
+		return err
+	}
+
 	err := models.DB.Model(&models.Channel{}).Where("id = ?", context.Param("id")).Delete(&models.Channel{}).Error
 
 	if err != nil {
